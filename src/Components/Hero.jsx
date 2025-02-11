87a06174eaf0e7a4f6e4cd730be5c9d8f80ae474
tjay1760 <mcjthiongo@gmail.com>
2025-02-11 16:47:38 +0300

    add custom fonts to About, Contact, Hero, and OurTeam components; enhance layout and styling

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,9 +7,7 @@ const Hero = () => {
     <div
       className="home relative h-screen bg-cover bg-center"
       style={{
-        backgroundImage: `url(${
-          window.innerWidth >= 1024 ? BackgroundImgDsk : BackgroundImg
-        })`,
+        backgroundImage: `url(${window.innerWidth >= 1024 ? BackgroundImgDsk : BackgroundImg})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
@@ -18,17 +16,19 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black/50" />
 
       {/* Content container */}
-      <div className="relative h-full  flex-col justify-center items-center text-white">
+      <div className="relative h-full flex-col justify-center items-center text-white">
         <div className="text-content md:w-2/3 flex flex-col items-center justify-center mt-20 md:mt-48 md:ml-20 md:p-3 absolute">
-        <h1 className=" bubblegum-sans-regular font-bold text-7xl text-center px-4 z-10 md:mb-4">
-          Little Readers are Little Leaders.
-        </h1>
-        <p1 className="comic-neue-regular text-xl font-semibold text-center px-4">
-          Inspire a love for reading and a passion for change with our global
-          library of diverse student stories
-        </p1>
+          <h1 className="bubblegum-sans-regular font-bold text-7xl text-center px-4 z-10 md:mb-4">
+            Little Readers are Little Leaders.
+          </h1>
+          <p className="comic-neue-regular text-xl font-semibold text-center px-4">
+            Inspire a love for reading and a passion for change with our global
+            library of diverse student stories
+          </p>
+          <button className="mt-6 px-6 py-3 bg-yellow-500 hover:bg-yellow-600 text-black font-bold rounded-lg shadow-md">
+            Sign Up
+          </button>
         </div>
-        
       </div>
     </div>
   );
