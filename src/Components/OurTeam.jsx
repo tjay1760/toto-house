fb16d994ab996a74bc480728aeb199f760ff1cb9
tjay1760 <mcjthiongo@gmail.com>
2025-02-11 15:55:17 +0300

    add book section to About component and adjust team member photo sizes

--- src/Components/OurTeam.jsx
+++ src/Components/OurTeam.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import person1 from "../assets/nandi-photo-crp.jpeg"
 import person2 from "../assets/winnie-photo-crp.jpeg"
 
+
+
 const OurTeam = () => {
   return (
     <div className="team min-h-screen w-full bg-gradient-to-b from-cyan-500 to-blue-500 flex flex-col">
@@ -16,14 +18,14 @@ const OurTeam = () => {
           <div className="md:flex justify-center gap-14">
             {/* Person 1 */}
             <div className='flex flex-col items-center gap-2'>
-              <img src={person1} className='h-40 w-40 md:h-96 md:w-96  rounded-full border-8 border-pink-600' alt='person1' />
+              <img src={person1} className='h-40 w-40 md:h-80 md:w-80  rounded-full border-8 border-pink-600' alt='person1' />
               <h2 className="text-xl font-bold">Nandi Fernandez</h2>
               <p>CEO</p>
             </div>
 
             {/* Person 2 */}
             <div className='flex flex-col items-center gap-2'>
-              <img src={person2} className='h-40 w-40 md:h-96 md:w-96 rounded-full border-8 border-pink-600' alt='person2' />
+              <img src={person2} className='h-40 w-40 md:h-80 md:w-80 rounded-full border-8 border-pink-600' alt='person2' />
               <h2 className="text-xl font-semibold">Winnie Mangeni</h2>
               <p className='font-bold text-xl'>CTO</p>
             </div>
