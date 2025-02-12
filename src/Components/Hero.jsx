ad340762ca725d690165af5dd5701310c0877a26
tjay1760 <mcjthiongo@gmail.com>
2025-02-12 10:58:35 +0300

    fix: update Hero component background image and adjust heading size for better visibility

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BackgroundImg from "../assets/hero_reader.jpg";
+import BackgroundImg from "../assets/hero-header.jpeg";
 import BackgroundImgDsk from "../assets/hero-header.jpeg";
 
 const Hero = () => {
@@ -16,8 +16,8 @@ const Hero = () => {
     >
       {/* Content container */}
       <div className="relative h-full flex-col justify-center items-center text-white">
-        <div className="text-content md:w-2/4 flex flex-col items-center justify-center mt-20 md:mt-48 md:ml-48 md:p-3 absolute">
-          <h1 className="font-bold text-7xl text-center px-4 z-10 md:mb-4">
+        <div className="text-content md:w-2/4 flex flex-col items-center justify-center mt-10 md:mt-48 md:ml-48 md:p-3 absolute">
+          <h1 className="font-bold text-5xl text-center px-4 z-10 md:mb-4">
             Little Readers are Little Leaders.
           </h1>
           <p className="text-xl font-semibold text-center px-4">
