15b848ece4c735f29387e40d543216bdb2ed0386
tjay1760 <mcjthiongo@gmail.com>
2025-02-19 15:04:07 +0300

    hero header edits

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BackgroundImg from "../assets/hero-header.jpeg";
+import BackgroundImg from "../assets/Hero Mobile.jpeg";
 import BackgroundImgDsk from "../assets/hero-header.jpeg";
 
 const Hero = () => {
@@ -8,7 +8,7 @@ const Hero = () => {
       className="home relative h-screen bg-cover bg-center"
       style={{
         backgroundImage: `url(${
-          window.innerWidth >= 1024 ? BackgroundImgDsk : BackgroundImg
+          window.innerWidth >= 768 ? BackgroundImgDsk : BackgroundImg
         })`,
         backgroundSize: "cover",
         backgroundPosition: "center",
@@ -16,11 +16,11 @@ const Hero = () => {
     >
       {/* Content container */}
       <div className="relative h-full flex-col justify-center items-center text-white">
-        <div className="text-content md:w-2/4 flex flex-col items-center justify-center mt-10 md:mt-48 md:ml-48 md:p-3 absolute">
-          <h1 className="font-bold text-5xl text-center px-4 z-10 md:mb-4">
+        <div className="text-content md:w-2/4 flex flex-col items-center justify-center mt-10 md:mt-10 md:ml-32 lg:mt-28 lg:ml-48 md:p-3 absolute">
+          <h1 className="font-bold text-3xl mt-8 md:text-5xl text-center px-4 z-10 md:mb-3">
             Little Readers are Little Leaders.
           </h1>
-          <p className="text-xl font-semibold text-center px-4">
+          <p className="md:text-2xl text-base md:w-2/3 font-semibold text-center px-4">
             Inspire a love for reading and a passion for change with our global
             library of diverse student stories
           </p>
